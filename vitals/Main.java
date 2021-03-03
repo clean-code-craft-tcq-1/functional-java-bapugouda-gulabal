63b88fae053b0087979b32a764a024a713b7933c
Bapugouda Gulabal <38274563+bapugouda-gulabal@users.noreply.github.com>
2021-03-03 13:58:01 +0530

    Add files via upload

--- vitals/Main.java
+++ vitals/Main.java
@@ -11,16 +11,14 @@ public class Main {
     	 
     	assert(lionBattery.isBatteryOk(25, 70, 0.7f) == true); 
         assert(lionBattery.isBatteryOk(50, 85, 0.0f) == false);
-        
         assert(lionBattery.isBatteryOk(25, 20, 0.9f) == false); 
         assert(lionBattery.isBatteryOk(25, 55, 0.9f) == false);
         assert(lionBattery.isBatteryOk(55, 65, 0.7f) == false);
         assert(lionBattery.isBatteryOk(15, 85, 0.7f) == false);
         assert(lionBattery.isBatteryOk(45, 15, 0.7f) == false);
-        assert(lionBattery.isBatteryOk(15, 25, 0.9f) == false);
-        assert(lionBattery.isBatteryOk(45, 80, 0.8f) == true);
-        assert(lionBattery.isBatteryOk(50 , 90, 0.9f) == false);
-        assert(lionBattery.isBatteryOk(-1 , 40, 0.8f) == false);
+        assert(lionBattery.isBatteryOk(45, 75, 0.7f) == true);
+        assert(lionBattery.isBatteryOk(50, 90, 0.9f) == false);
+        assert(lionBattery.isBatteryOk(-1, 40, 0.8f) == false);
     	                
        System.out.println("Testing Finished!!");
     }
